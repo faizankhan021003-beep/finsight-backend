cb734d73e24652fe628a561d1a7562da3116b056
Faizan khan <faizankhan021003-beep>
2026-08-04 14:57:53 +0530

    Create React login page with state management

--- finsight-frontend/src/pages/Login.jsx
+++ finsight-frontend/src/pages/Login.jsx
@@ -1,8 +1,31 @@
+import { useState } from "react";
+import "../styles/Login.css";
+
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
-    <div>
-      <h2>Login Page</h2>
-      <p>This is our first React page.</p>
+    <div className="login-container">
+      <h2>Login</h2>
+
+      <input
+        type="email"
+        placeholder="Enter Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+
+      <input
+        type="password"
+        placeholder="Enter Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+
+      <button onClick={() => console.log(email, password)}>
+        Login
+      </button>
     </div>
   );
 }
